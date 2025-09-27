eb5643f94e7e6fe9cf30c787f52b605103df68b8
anttonalberdi <anttonalberdi@gmail.com>
2025-09-27 10:00:53 +0200

    init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     author="Antton Alberdi",
     author_email="antton.alberdi@sund.ku.dk",
-    description="Python software for simulating holo-omic datasets",
+    description="Flexible software for simulating holo-omic datasets",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
